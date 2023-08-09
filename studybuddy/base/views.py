d8ebd868cf98718f88fcf5500c8a2a76d8938708
Naman Naman <nbatra752@gmail.com>
2023-08-09 14:00:03 +0530

    Test bug of login

--- studybuddy/base/views.py
+++ studybuddy/base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def logInPage(request):
@@ -50,10 +50,6 @@ def userProfile(request, pk):
     return render(request, 'base/profile.html', context)
 
 
-def registerPage(request):
-    page = 'register'
-    context = {'page': page}
-    return render(request, 'base/login_register.html', context)
 
 
 def home(request):
@@ -98,35 +94,49 @@ def room(request, pk):
 
 @login_required(login_url='login')
 def createRoom(request):
+    action = "Create"
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name = request.POST.get('name'),
+            description = request.POST.get('description')
+        )
+        return redirect('home')
+    context = {'form': form, "topics" : topics, "action" : action}
     return render(request, 'base/room_form.html', context)
 
 
 @login_required(login_url='login')
 def updateRoom(request, pk):
+    action = "Update"
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
 
     if request.user != room.host:
         return HttpResponse('You are not allowed here!!')
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
+        
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        room.name = request.POST.get('name')
+        room.topic = topic
+        room.description = request.POST.get('description')
+        room.save()    
+        return redirect('home')
+    context = {'form': form, "room" :room, "topics" : topics, "action" : action}
     return render(request, 'base/room_form.html', context)
 
 
 def registerPage(request):
+    page = 'register'
+    context = {'page': page}
     form = UserCreationForm()
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
@@ -138,27 +148,42 @@ def registerPage(request):
             return redirect('home')
         else:
             messages.error(request, 'An error occurred during registration')
-    return render(request, 'base/login_register.html', {'form': form})
+    return render(request, 'base/login_register.html', {'form': form, 'page': page})
 
 
 @login_required(login_url='login')
 def deleteRoom(request, pk):
+    action = "room"
     room = Room.objects.get(id=pk)
     if request.user != room.host:
         return HttpResponse('You are not allowed here')
     if request.method == 'POST':
         room.delete()
         return redirect('home')
-    context = {'obj': room}
+    context = {'obj': room, 'action' : action}
     return render(request, 'base/delete.html', context)
 
 
 @login_required(login_url='login')
 def deleteMessage(request, pk):
+    action = "message"
     message = Message.objects.get(id=pk)
     if request.user != message.user:
         return HttpResponse("You are not allowed to delete")
     if request.method == 'POST':
         message.delete()
         return redirect('home')
-    return render(request, 'base/delete.html', {'obj': message})
+    return render(request, 'base/delete.html', {'obj': message, 'action' : action})
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+    
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk = user.id)
+    context = {'form' : form}
+    return render(request, 'base/update-user.html', context)
